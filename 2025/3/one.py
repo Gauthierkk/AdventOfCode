b2be845279821a1f7afa5bc21315e450fb1b72b8
gauthier <gkelly41@gatech.edu>
2025-12-16 09:20:27 +0100

    day 3 trial

--- 2025/3/one.py
+++ 2025/3/one.py
@@ -3,7 +3,36 @@ from pathlib import Path
 # Read input file from the same directory as this script
 input_file = Path(__file__).parent / 'input.txt'
 with open(input_file, 'r') as f:
-    data = f.read().strip()
+    lines = f.readlines()
+    sum = 0
 
-# Your solution here
-print(data)
+    for line in lines:
+        line = line.strip()
+
+        j = 1
+        a = line[0]
+        b = line[j]
+
+        max_num = 0
+        print(f"Initial max_num: {max_num}")
+
+        for i in range(len(line)-1):
+            if i == j:
+                j += 1
+                continue
+
+            while int(a + b) <= max_num and j < len(line)-1:
+                j += 1
+                b = line[j]
+                if int(a + b) > max_num:
+                    max_num = int(a + b)
+                    print(max_num)
+                    break
+        
+        print(f"Final max num: {max_num}")
+        sum += max_num
+    
+    print(f"Final Sum: {sum}")
+
+            
+        
